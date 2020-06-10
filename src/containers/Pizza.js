54f2bece6960abebd2900cb20bc3fdd49cef09d9
marita30 <marita.jose.ch@gmail.com>
2020-06-10 14:37:41 -0400

    Agregando dos archivos llamados Pizza.js y Users.js

---
+++ src/containers/Pizza.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './PizzaImage.css';
+
+const pizzaImage = (props) => (
+    <div className={classes.pizzaImage}>
+
+    </div>
+);
+
+
+export default pizzaImage;
