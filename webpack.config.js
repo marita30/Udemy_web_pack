4e603dbfadceeea1ce842fccaf5b5412866e0e16
marita30 <marita.jose.ch@gmail.com>
2020-06-11 14:14:22 -0400

    Despues de instalar los paquetes de css en el package.json , configuramos aqui pra que cargue ahora los archivos css y luego vamos al archivo package.json a agregar una linea de codigo que esta en el archivo .babelcr dentro de targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const autoprefixer = require('autoprefixer');
+
 
 module.exports =  {
     mode: 'development',
@@ -13,10 +15,35 @@ module.exports =  {
     devtools: 'cheap-module-eval-source-map',
     module: {
         rules: [
+
             {
-                test: /\.js&/,
+                test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use:[
+                 { loader: 'style-loader'},
+                   {
+                     loader:  'css-loader', 
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]'
+                            }
+                        } 
+                    },
+
+                    { loader: 'postcss-loader', 
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [autoprefixer()]
+                        } 
+                    }
+                ]
             }
         ]
     }
